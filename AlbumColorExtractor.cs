3c97a226f54f203fd407cabd2c325bb0f9e853df
raula09 <davitoniani0602@gmail.com>
2025-11-17 13:29:01 +0400

    design + fullscreen update

---
+++ AlbumColorExtractor.cs
@@ -0,0 +1,52 @@
+using Avalonia;
+using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using System;
+
+namespace MusicPlayerApp
+{
+    public static class AlbumColorExtractor
+    {
+        public static Color Extract(Bitmap bmp)
+        {
+            var size = new PixelSize(24, 24);
+            var rtb = new RenderTargetBitmap(size);
+
+            using (var dc = rtb.CreateDrawingContext())
+                dc.DrawImage(bmp, new Rect(bmp.Size), new Rect(0, 0, size.Width, size.Height));
+
+            int pixels = size.Width * size.Height;
+            int stride = size.Width * 4;
+            byte[] buf = new byte[pixels * 4];
+
+            var handle = System.Runtime.InteropServices.GCHandle.Alloc(buf, System.Runtime.InteropServices.GCHandleType.Pinned);
+            try
+            {
+                rtb.CopyPixels(
+                    new PixelRect(0, 0, size.Width, size.Height),
+                    handle.AddrOfPinnedObject(),
+                    buf.Length,
+                    stride);
+            }
+            finally
+            {
+                handle.Free();
+            }
+
+            long rs = 0, gs = 0, bs = 0;
+
+            for (int i = 0; i < buf.Length; i += 4)
+            {
+                bs += buf[i + 0];
+                gs += buf[i + 1];
+                rs += buf[i + 2];
+            }
+
+            byte r = (byte)(rs / pixels);
+            byte g = (byte)(gs / pixels);
+            byte b = (byte)(bs / pixels);
+
+            return Color.FromRgb(r, g, b);
+        }
+    }
+}
